9c7ecbf83741284e3bb11b26453470f029ee006d
thatsfov1 <zheniaukr@icloud.com>
2023-02-27 19:09:02 +0200

    try to handle cors issue

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:'/api/1.0/',
+    baseURL:'',
     withCredentials:true,
     headers: {
         "API-KEY": process.env.REACT_APP_API_KEY
